3895c38a8e738f8e1706ee8df88627c700938d5e
Shan <ramanean@gmail.com>
2015-09-13 13:57:04 +0530

    DOM Functions
    DOM functions in Php for extracting data from any page

---
+++ domfunctions.php
@@ -0,0 +1,55 @@
+<?Php
+
+//Getting the contents of the url and extracting data from it using Custom functions
+
+$website_url='http://www.spacenext.com';
+$curl = curl_init($website_url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.10 (KHTML, like Gecko) Chrome/8.0.552.224 Safari/534.10');
+$html = curl_exec($curl);
+curl_close($curl);
+
+
+//Dom functions
+$dom = new DOMDocument();
+@$dom->loadHTML($html);
+$xpath = new DOMXPath($dom);
+
+
+function elementfind($xpath,$query)
+{
+
+	$rows = $xpath->query($query);
+	$array = array();
+	foreach($rows as $node){
+		$array[] = $node;
+	}
+	return $array;
+}
+
+//Finding only text - Usage textfind($xpath,$query,"text") - query is nothing but any xpath query.
+function textfind($xpath,$query,$att1)
+{
+
+	$rows = $xpath->query($query);
+	$array = array();
+	foreach($rows as $node){
+		$array[] = $node->textContent;
+	}
+	return $array;
+}
+
+//Finding only links - Usage textfind($xpath,$query,"href")
+function linkfind($xpath,$query,$attribute)
+{
+
+	$rows = $xpath->query($query);
+	$array = array();
+	foreach($rows as $node){
+		$array[] = $node->getAttribute($attribute);
+	}
+	return $array;
+}
+
+?>
