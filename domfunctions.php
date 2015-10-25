a31bbc3eaf6ddc04ac68df69c826fcda0b7dd010
Shan <ramanean@gmail.com>
2015-10-25 12:38:18 +0530

    Update domfunctions.php
    Added a new get_dom function

--- domfunctions.php
+++ domfunctions.php
@@ -17,6 +17,24 @@ $dom = new DOMDocument();
 $xpath = new DOMXPath($dom);
 
 
+function get_dom($url)
+{
+	$website_url='http://www.spacenext.com';
+	$website_url=$url;
+	$curl = curl_init($website_url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.10 (KHTML, like Gecko) Chrome/8.0.552.224 Safari/534.10');
+	$html = curl_exec($curl);
+	curl_close($curl);
+	//Dom functions
+	$dom = new DOMDocument();
+	@$dom->loadHTML($html);
+	$xpath = new DOMXPath($dom);
+	return $xpath;
+}
+
+
 function elementfind($xpath,$query)
 {
 
